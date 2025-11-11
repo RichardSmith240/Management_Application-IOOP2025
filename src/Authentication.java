7872180434db884867a69b585d5fc29d18bc51f5
psony3 <Psony@rockets.utoledo.edu>
2025-11-10 22:37:02 -0500

    Added authentication login

--- src/Authentication.java
+++ src/Authentication.java
@@ -1,10 +1,25 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Authentication{
 	//authenication method
-	public static boolean Authenticate(){
-		return true;
+	public boolean Authenticate(){
+		Scanner scnr = new Scanner(System.in);
+		System.out.print("Enter username: ");
+		String username = scnr.nextLine();
+		System.out.print("Enter password: ");
+		String password = scnr.nextLine();
+		//searches for username and password
+		for(int i=0; i<usernames.size(); i++){
+			if(usernames.get(i).equals(username) && passwords.get(i).equals(password)){
+				System.out.println("Login successful! Welcome, " + username + ".");
+				return true;
+			}
+		}
+		System.out.println("Invalid username or password.");
+		return false;
 	}
+
 	//sign-up method
 	public static void SignUp(){
 		System.out.println("lol");
